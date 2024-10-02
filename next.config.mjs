6cbdc2573eb8675cec43e9175cfd84e9427ea44c
Tayyaba Tariq <tayyaba2031@gmail.com>
2024-10-02 14:55:49 +0500

    final push 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
-modules.exports = {
+const nextConfig = {
   images: {
     unoptimized: true,
     domains: ["vercel.com"], // Add any external domains you're loading images from
   },
+  // You can add more configuration options here as needed
 };
 
 export default nextConfig;
